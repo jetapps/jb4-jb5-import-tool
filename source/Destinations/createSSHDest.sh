997fed7f9224dbba40ff2878b09632ce28a6af07
JetAppsRichard <78180564+JetAppsRichard@users.noreply.github.com>
2021-05-26 16:14:36 -0700

    Update createSSHDest.sh
    Make the SSH Key Dynamic

--- source/Destinations/createSSHDest.sh
+++ source/Destinations/createSSHDest.sh
@@ -10,8 +10,8 @@
 
 ###### Global Variables ######
 FILEPATH=$(pwd)/source/files
-JB4ARGS=( "name" "dr" "hidden" "path" "disklimit" "rsyncbwlimit" "host" "port" "username" "timeout" "rsyncpreferip")
-SSHKEY=/usr/local/jetapps/etc/jetbackup/privatekeys/root.rsa
+JB4ARGS=( "name" "dr" "hidden" "path" "disklimit" "rsyncbwlimit" "host" "port" "username" "timeout" "rsyncpreferip" "privatekey")
+JB4SSHKEY=/usr/local/jetapps/etc/jetbackup/privatekeys/root.rsa
 JB5SSHKEY=/usr/local/jetapps/etc/jetbackup5/privatekeys/root.rsa
 
 source $(pwd)/source/functions.sh
@@ -38,23 +38,25 @@ function createSSHDestination () {
     then
       VAL=0
     fi
-    echo "${key} : ${VAL}" >> $workspace
     ARGS[${key}]=${VAL}
+    #Handle the private key
+    if [[ -f "${ARGS['privatekey']}" ]]; then
+      SSHKEY="${ARGS['privatekey']}"
+    else
+      cp $JB4SSHKEY $JB5SSHKEY
+      SSHKEY=$JB5SSHKEY
+      ARGS['privatekey']=$SSHKEY
+    fi
+    echo "${key} : ${ARGS[$key]}" >> $workspace
+    
   done
   echo "disabled : 1" >> $workspace
-  echo "privatekey: $JB5SSHKEY" >> $workspace
   echo "" >> $workspace
   echo "We will be using a private SSH Key generated from JetBackup 4 to reauthenticate the SSH Destination ${NAME}." >> $workspace
   #printDefaultDisabled
 
-if [[ -f "{JB5SSHKEY}" ]]; then
-  continue
-else
-  cp $SSHKEY $JB5SSHKEY
-fi
-
 ########## building command ###############
-CMD="jetbackup5api -F manageDestination -D 'action=create&type=SSH&name=${ARGS[name]}&disabled=1&dr=${ARGS[dr]}&hidden=${ARGS[hidden]}&options[path]=${ARGS[path]}&disk_limit=${ARGS[disklimit]}&options[rsyncbwlimit]=${ARGS[rsyncbwlimit]}&options[host]=${ARGS[host]}&options[port]=${ARGS[port]}&options[privatekey]=${JB5SSHKEY}&options[username]=${ARGS[username]}'"
+CMD="jetbackup5api -F manageDestination -D 'action=create&type=SSH&name=${ARGS[name]}&disabled=1&dr=${ARGS[dr]}&hidden=${ARGS[hidden]}&options[path]=${ARGS[path]}&disk_limit=${ARGS[disklimit]}&options[rsyncbwlimit]=${ARGS[rsyncbwlimit]}&options[host]=${ARGS[host]}&options[port]=${ARGS[port]}&options[privatekey]=${SSHKEY}&options[username]=${ARGS[username]}'"
 #eval $CMD
 echo $CMD >> $workspace
 }
